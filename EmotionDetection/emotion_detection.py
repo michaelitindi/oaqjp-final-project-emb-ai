39a2d9a46b27fdaa894d5bc8dc97cdc9900532cc
michaelitindi <michaelitindi@gmail.com>
2024-08-08 14:00:15 -0400

    Formatting response

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,8 +6,9 @@ def emotion_detector(text_to_analyze):
     myobj = { "raw_document": { "text": text_to_analyze } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    #formatted_response = json.loads(response)
-    #emotion = formatted_response[''][0]['emotion']
-    return response
+    formatted_response = json.loads(response)
+    emotion = formatted_response['emotionPredictions'][0]['emotion']
+    max_key = max(emotion, key = emotion.get)
+    #return emotion
 
 
