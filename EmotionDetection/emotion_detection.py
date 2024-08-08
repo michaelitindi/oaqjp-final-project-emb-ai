dc899efcad28d682880b93a7eca18353bf6b6764
michaelitindi <michaelitindi@gmail.com>
2024-08-08 13:32:33 -0400

    emotion detection

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,13 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    myobj = { "raw_document": { "text": text_to_analyze } }
+    header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
+    response = requests.post(url, json = myobj, headers=header)
+    #formatted_response = json.loads(response)
+    #emotion = formatted_response[''][0]['emotion']
+    return response
+
+
